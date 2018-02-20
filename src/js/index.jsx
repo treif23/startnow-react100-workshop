c5248bfdca3f6d99508e555bae8943c150f4fdc8
Tommy <treif23@github.com>
2018-02-20 13:18:44 -0800

    Index update

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -2,18 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Square extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      value: null,
+    };
+  }
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button className="square" onClick={() => this.setState({value : 'X'})}>
+        {this.state.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      squares: Array(9).fill(null),
+    };
+  }
   renderSquare(i) {
-    return <Square />;
+    return <Square value = {i} />;
   }
 
   render() {
